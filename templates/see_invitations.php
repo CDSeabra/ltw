8975357602c65c8422dd987976d9308223bc4d68
CarolinaMoreira <carolm@sapo.pt>
2015-12-06 14:55:53 +0000

    ...

--- templates/see_invitations.php
+++ templates/see_invitations.php
@@ -16,6 +16,7 @@
 			<h3><a href="single_event.php?id=<?=$row['id_event']?>">Event's name: <?= $row['name'] ?></a></h3> <br>
 			<? $image_name = getImage($row['id_event']); ?>
 			<img src="images/<?="$image_name"?>" alt="Event's photo">
+			<p>
 			<span class="type">Type: <?=$row['tipo']?></span><br>
 			<span>Description: <?=$row['description']?></span><br>
 			<span>Place: <?=$row['place']?> </span><br>
@@ -23,6 +24,7 @@
 			<span>Initial time: <?=$row['time_init']?></span><br>
 			<span>Final time: <?=$row['time_end']?></span><br>
 			<span>Privacy: <? if($row['privado'] == 'true') echo 'Private'; else echo 'Public' ;?></span>
+			</p>
 			
 			<!-- GOD'S GIFT -->
 			<div style="clear: both"></div>
